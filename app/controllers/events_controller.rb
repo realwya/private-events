d056cf0f4701201ee708ef98ea5e4ecdd25c4ad4
Eddie Wen <405139216@qq.com>
2024-06-20 13:58:26 +0800

    complete event and user model

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,30 @@
+class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      redirect_to events_path, notice: 'Event was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def post_params
+      params.require(:event).permit(:name, :date, :location)
+    end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  has_many :created_events, foreign_key: :creator_id, class_name: "Event"
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: {in: 2..20 }, uniqueness: true
+end
