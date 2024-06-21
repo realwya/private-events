67a312717feaafa9d6206e01e58eea5d3621bf67
Eddie Wen <405139216@qq.com>
2024-06-21 17:01:48 +0800

    added through table

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
+  has_many :attendees, through: :event_attendances, source: :event_attendee
+  has_many :event_attendances, foreign_key: :attended_event_id
 
   validates :name, presence: true
   validates :location, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :created_events, foreign_key: :creator_id, class_name: "Event"
+  has_many :attended_events, through: :event_attendances
+  has_many :event_attendances, foreign_key: :event_attendee_id
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
