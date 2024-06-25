49d2c584454a0a1299e8f65daf63109c7aaf576b
Eddie Wen <405139216@qq.com>
2024-06-25 11:13:05 +0800

    added attended events to user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_many :created_events, foreign_key: :creator_id, class_name: "Event"
-  has_many :attended_events, through: :event_attendances
   has_many :event_attendances, foreign_key: :event_attendee_id
+  has_many :attended_events, through: :event_attendances
+
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
